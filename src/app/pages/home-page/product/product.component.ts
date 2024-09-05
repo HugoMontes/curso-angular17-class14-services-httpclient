089942751ba47e25d43ee1b8f9645d1523eda31f
hmontes <hugomontes927@gmail.com>
2024-09-05 18:14:07 -0400

    First commit

---
+++ src/app/pages/home-page/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+	selector: 'app-product',
+	standalone: true,
+	imports: [MatCardModule, MatButtonModule],
+	templateUrl: './product.component.html',
+	styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+	clickAddToCard(): void {}
+}
