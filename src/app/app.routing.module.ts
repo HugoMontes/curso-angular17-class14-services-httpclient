8ccb24a6460b65deee31905d173f6afea606b660
hmontes <hugomontes927@gmail.com>
2024-09-12 18:42:35 -0400

    114. Envió de datos entre rutas – Path y Query Param

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: LoginPageComponent,
   },
   {
-    path: 'payment',
+    path: 'payment/:user',
     component: PaymentPageComponent,
     children: [
       {
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -10,39 +10,56 @@ import { IApiResponseProduct } from '../../services/models/product-api.interface
 import { CartService } from '../../services/cart.service';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
-	selector: 'app-home-page',
-	standalone: true,
-	imports: [MatToolbar, MatIcon, MatButtonModule, MatBadgeModule, MatSidenavModule, ProductComponent, AsyncPipe, RouterLink],
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatButtonModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    ProductComponent,
+    AsyncPipe,
+    RouterLink,
+  ],
 
-	templateUrl: './home-page.component.html',
-	styleUrl: './home-page.component.scss'
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent implements OnInit {
+	
+  count = 0;
+  private readonly _productsApiService = inject(ProductsApiService);
+  // private readonly _cartService = inject(CartService);
+  readonly cartService = inject(CartService);
+  private readonly _activatedRoute = inject(ActivatedRoute);
 
-	count = 0;
-	private readonly _productsApiService = inject(ProductsApiService);
-	// private readonly _cartService = inject(CartService);
-	readonly cartService = inject(CartService);
+  products: IApiResponseProduct[] = [];
+  products$!: Observable<IApiResponseProduct[]>;
 
-	products: IApiResponseProduct[] = [];
-	products$!: Observable<IApiResponseProduct[]>;
+  ngOnInit(): void {
+    // this._productsApiService.getProducts().subscribe((data) => console.log(data));
+    // this._productsApiService.getProducts().subscribe((data) => this.products = data);
+    this._getApis();
+    this._getValueRoutes();
+  }
 
-	ngOnInit(): void {
-		// this._productsApiService.getProducts().subscribe((data) => console.log(data));
-		// this._productsApiService.getProducts().subscribe((data) => this.products = data);
-		this._getApis()
-		
-		// this._cartService.cartObservable$.subscribe({
-		// 	next: (number) => {
-		// 		this.count = number;
-		// 	}
-		// });
-	}
+  private _getValueRoutes() {
+    console.log(
+      'Valores obtenidos por QueryParam ==>',
+      this._activatedRoute.snapshot.queryParams
+    );
+  }
 
-	private _getApis() {
-		this.products$ = this._productsApiService.getProducts();
-	}
+  private _getApis() {
+    this.products$ = this._productsApiService.getProducts();
+	// this._cartService.cartObservable$.subscribe({
+    // 	next: (number) => {
+    // 		this.count = number;
+    // 	}
+    // });
+  }
 }
