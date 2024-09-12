b44563fb753fd2b11c67064a2730cee8abaff54a
hmontes <hugomontes927@gmail.com>
2024-09-11 21:34:57 -0400

    110. Comodines Routes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: '404', component: NotFoundPageComponent },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  // { path: '404', component: NotFoundPageComponent },
+  // { path: '**', redirectTo: '/404', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },       // http://localhost:4200/
+  { path: 'login', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundPageComponent },
 ];
 
 @NgModule({
