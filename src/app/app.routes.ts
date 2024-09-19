d9243337af80c13f7726208ef94b185974d70417
hmontes <hugomontes927@gmail.com>
2024-09-19 18:08:12 -0400

    123. Lazy Loading MODERNO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-// import { LoginPageComponent } from './pages/login-page/login-page.component';
-// import { PaymentPageComponent } from './pages/payment-page/payment-page.component';
-// import { SimpleProductDetailPageComponent } from './pages/payment-page/simple-product-detail-page/simple-product-detail-page.component';
-// import FullProductDetailsPageComponent from './pages/payment-page/full-product-details-page/full-product-details-page.component';
-// import { ProductsResolverService } from './services/products.resolver';
+import { ProductsResolverService } from './services/products.resolver';
 
 export default [
   { path: 'home', title: 'Home', component: HomePageComponent },
@@ -15,27 +11,12 @@ export default [
     // component: LoginPageComponent,
     loadChildren: () => import('./pages/login-page/login-page.module').then((m) => m.LoginPageModule)
   },
-  // {
-  //   path: 'payment/:user',
-  //   component: PaymentPageComponent,
-  //   data: { title: 'Pagos' },
-  //   resolve: { products: ProductsResolverService },
-  //   children: [
-  //     {
-  //       path: 'simple-product-detail', // http://localhost:4200/payment/simple-product-detail
-  //       component: SimpleProductDetailPageComponent,
-  //     },
-  //     {
-  //       path: 'full-product-details', // http://localhost:4200/payment/full-product-details
-  //       component: FullProductDetailsPageComponent,
-  //     },
-  //     {
-  //       path: '',
-  //       pathMatch: 'full',
-  //       redirectTo: 'simple-product-detail',
-  //     },
-  //   ],
-  // },
+  {
+    path: 'payment/:user',
+    data: { title: 'Pagos' },
+    resolve: { products: ProductsResolverService },
+    loadChildren: () => import('./pages/payment-page/payment-page.routes'),
+  },
 
   //SE RECOMIENDA PONER A FINAL DE LAS RUTAS EL USO DE COMODINES
   // { path: '404', component: NotFoundPageComponent },
---
+++ src/app/pages/payment-page/payment-page.routes.ts
@@ -0,0 +1,25 @@
+import { PaymentPageComponent } from './payment-page.component';
+import { SimpleProductDetailPageComponent } from './simple-product-detail-page/simple-product-detail-page.component';
+import { Routes } from '@angular/router';
+
+export default [
+  {
+    path: '',
+    component: PaymentPageComponent,
+    children: [
+      {
+        path: 'simple-product-detail', // http://localhost:4200/payment/simple-product-detail
+        component: SimpleProductDetailPageComponent,
+      },
+      {
+        path: 'full-product-details', // http://localhost:4200/payment/full-product-details
+        loadComponent: () => import('./full-product-details-page/full-product-details-page.component'),
+      },
+      {
+        path: '', // http://localhost:4200/payment/simple-product-detail
+        pathMatch: 'full',
+        redirectTo: 'simple-product-detail',
+      },
+    ],
+  },
+] as Routes;
