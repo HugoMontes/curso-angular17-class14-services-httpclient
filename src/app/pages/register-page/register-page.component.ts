60f7d820531d9ebc57a5c92070ad9891f50b0af1
hmontes <hugomontes927@gmail.com>
2024-10-02 17:01:56 -0400

    129. Validaciones

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
+import { last } from 'rxjs';
 
 @Component({
 	selector: 'app-register-page',
@@ -16,13 +17,17 @@ import { MatInput } from '@angular/material/input';
 export default class RegisterPageComponent {
 	
 	// formGroup = new FormGroup({
-	// 	names: new FormControl('')
+	// 	names: new FormControl('', {validators: Validators.required}),
+	// 	lastName: new FormControl('', {validators: Validators.required}),
+	// 	email: new FormControl('', {validators: [Validators.required, Validators.email]}),
 	// });
 
 	private readonly _formBuilder = inject(FormBuilder);
 
 	formGroup = this._formBuilder.nonNullable.group({
-		names: ''
+		names: ['', Validators.required],
+		lastName: ['', Validators.required],
+		email: ['', [Validators.required, Validators.email]],
 	});
 
 	clickRegister(): void{
