096597bd1365a513733867a1e3a54caae9197c0a
hmontes <hugomontes927@gmail.com>
2024-10-09 11:10:25 -0400

    139. Acceder a elementos del FormArray

--- src/app/pages/payment-page/simple-product-detail-page/simple-product-detail-page.component.ts
+++ src/app/pages/payment-page/simple-product-detail-page/simple-product-detail-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 interface ICartProduct {
 	name: string;
@@ -24,15 +24,19 @@ const PRODUCTS: ICartProduct[] = [
 	templateUrl: './simple-product-detail-page.component.html',
 	styleUrl: './simple-product-detail-page.component.scss'
 })
-export class SimpleProductDetailPageComponent {
+export class SimpleProductDetailPageComponent implements OnInit {
 	displayedColumns: string[] = ['name', 'price', 'quantity', 'total', 'action'];
-	dataSource = PRODUCTS;
+	// dataSource = PRODUCTS;
 	private readonly formBuilder = inject(FormBuilder);
-
 	form = this.formBuilder.group({
 		total: PRODUCTS.reduce((prev, current) => prev + current.total, 0),
 		products: this.formBuilder.array(PRODUCTS.map((item) =>this._createFormGroup(item))),
 	});	
+	dataSource = new MatTableDataSource(this.form.controls.products.controls);
+
+	ngOnInit(): void {
+		this._calculate_row_total();
+	}
 
 	private _createFormGroup(item: ICartProduct) {
 		return this.formBuilder.group({
@@ -41,5 +45,24 @@ export class SimpleProductDetailPageComponent {
 			quantity: item.quantity,
 			total: item.total
 		});
+	}
+
+	private _calculate_row_total() {
+		this.productsFormArray.controls.forEach(({controls: {quantity, price, total}}) => {
+			quantity.valueChanges.subscribe((value) => {
+				// console.log(value);
+				console.log("---- CAMBIO EN CANTIDAD....");
+				const priceValue = price.value!;
+				let totalValue = 0;
+				if(value) {
+					totalValue = priceValue * value;
+				}
+				total.patchValue(totalValue);
+			});
+		});
+	}
+
+	get productsFormArray() {
+		return this.form.controls.products;
 	}
 }
